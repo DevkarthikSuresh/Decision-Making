2cdc303359f4ec0ddd16d2ad2a46b8c605aaddff
DevkarthikSuresh <devkarthik300@gmail.com>
2024-07-31 15:08:38 +0530

    Update odd_even.cpp

---
+++ odd_even.cpp
@@ -0,0 +1,21 @@
+
+//Name: Devkarthik Suresh
+//PRN: 23070123045
+//AIM - To check whether a number is even or odd.
+#include <iostream>
+using namespace std;
+
+int main() { int a;
+  
+  cout << "Enter a number: ";
+  cin >> a;
+if (a%2 == 0)
+{ cout << "The number is even.";}
+else
+{cout << "The number is not even.";}
+    return 0;
+}
+
+/* output-
+Enter a number: 6
+The number is even.*/
