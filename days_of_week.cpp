96b8b38283468030a8a0eb4cc762ec7bf8d74b6a
DevkarthikSuresh <devkarthik300@gmail.com>
2024-07-31 16:12:45 +0530

    Update days_of_week.cpp

---
+++ days_of_week.cpp
@@ -0,0 +1,55 @@
+
+//Name: Devkarthik Suresh
+//PRN: 23070123045
+//AIM - To use switch case to print days of week.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int choice;
+  
+    cout << "Days of the Week" << endl;
+    cout << "1. Monday" << endl;
+    cout << "2. Tuesday" << endl;
+    cout << "3. Wednesday" << endl;
+    cout << "4. Thursday" << endl;
+    cout << "5. Friday" << endl;
+    cout << "6. Saturday" << endl;
+    cout << "7. Sunday" << endl;
+    cout << "Enter the number to get the day of the week: ";
+    cin >> choice;
+  
+    switch (choice) {
+        case 1:
+            cout << "You selected Monday." << endl;
+            break;
+        case 2:
+            cout << "You selected Tuesday" << endl;
+            break;
+        case 3:
+            cout << "You selected Wednesday." << endl;
+            break;
+        case 4:
+            cout << "You selected Thursday." << endl;
+            break;
+        case 5:
+            cout << "You selected Friday." << endl;
+            break;
+        case 6:
+            cout << "You selected Saturday." << endl;
+            break;
+        case 7:
+            cout << "You selected Sunday." << endl;
+            break;
+        default:
+            cout << "Invalid choice. Please enter a number between 1 and 7." << endl;
+            break;
+    }
+  
+    return 0;
+}
+
+/*Output-
+Enter a character: o
+o is a vowel
+*/
