505f441ab5269eed565db5867a6b962c606ef49e
DevkarthikSuresh <devkarthik300@gmail.com>
2024-07-31 15:35:53 +0530

    Add files via upload

---
+++ vowel checker.cpp
@@ -0,0 +1,23 @@
+
+//Name: Devkarthik Suresh
+//PRN: 23070123045
+//AIM - To check whether a number is even or odd.
+#include <iostream>
+using namespace std;
+
+int main() { char a;
+  
+  cout << "Enter a character: ";
+  cin >> a;
+  if (a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U')
+  {cout << a << " is a vowel";}
+  else
+  {cout << a << " is a consonant";}
+return 0;
+    
+}
+
+/*Output-
+Enter a character: o
+o is a vowel
+*/
